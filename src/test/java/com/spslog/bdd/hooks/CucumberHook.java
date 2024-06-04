db7458a6d1d763015127e8ee0c7f4896038d1929
Sreehari P S <92331340+sreeharipslog@users.noreply.github.com>
2024-06-04 14:43:53 +0530

    Increment1: support for page object model

--- src/test/java/com/spslog/bdd/hooks/CucumberHook.java
+++ src/test/java/com/spslog/bdd/hooks/CucumberHook.java
@@ -3,6 +3,9 @@ package com.spslog.bdd.hooks;
 import com.spslog.bdd.factory.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class CucumberHook {
@@ -13,8 +16,15 @@ public class CucumberHook {
         driver = DriverFactory.initializeDriver();
     }
 
-    @After
+    @After(order = 1)
+    public void takeScreenShot(Scenario scenario) {
+        byte[] image = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(image, "image/png", scenario.getName());
+    }
+
+    @After(order = 0)
     public void after() {
         driver.quit();
     }
+
 }
