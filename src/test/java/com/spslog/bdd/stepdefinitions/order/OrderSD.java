e3cb519fe5abe3c30cdcf79ee8bce7621aa71b5c
Sreehari P S <92331340+sreeharipslog@users.noreply.github.com>
2024-06-06 10:18:55 +0530

    Increment7: Using TestNG runner for scenario parallelism.

--- src/test/java/com/spslog/bdd/stepdefinitions/order/OrderSD.java
+++ src/test/java/com/spslog/bdd/stepdefinitions/order/OrderSD.java
@@ -10,7 +10,8 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.Assert;
+// import org.junit.Assert;
+import org.testng.Assert;
 import org.openqa.selenium.WebDriver;
 
 import java.util.List;
